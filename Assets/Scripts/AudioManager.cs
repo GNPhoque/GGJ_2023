88ff0b05ad78e13799f0c3044de7e3dd5e869f40
Azgoum <34266367+Azgoum@users.noreply.github.com>
2023-02-05 17:09:45 +0100

    Commit aussi

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource loopAudio;
+    [SerializeField] AudioSource audioSource;
+
+    [SerializeField] AudioClip[] sfx;
+    [SerializeField] AudioClip music;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance) Destroy(instance);
+
+        instance = this;
+        DontDestroyOnLoad(this);
+
+        PlayMusic();
+
+    }
+
+    public void PlayMusic()
+    {
+        loopAudio.PlayOneShot(music);
+    }
+
+    public void PlayHeartSound() => audioSource.PlayOneShot(sfx[0]);
+    public void PlayTryAgain() => audioSource.PlayOneShot(sfx[1]);
+    public void PlayWin() => audioSource.PlayOneShot(sfx[2]);
+    public void PlayRootMove() => audioSource.PlayOneShot(sfx[3]);
+
+}
